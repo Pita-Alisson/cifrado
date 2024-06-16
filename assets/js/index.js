efc6fec6f190aa936f638fe8fd6ac66d429bfefa
Pita-Alisson <alissonpita622@gmail.com>
2024-06-15 23:26:39 -0400

    nuevoCambios

---
+++ assets/js/index.js
@@ -0,0 +1,135 @@
+import cipher from './cipher.js';
+// Se realiza el uso del metodo addEventListener("DOMContentLoaded") para realizar una carga en nuestro DOM antes del script
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Manejo de la página de index.html
+    // Creamos una variable para almacenar y manejar el formulario de registro en index.html
+    var registroForm = document.getElementById('registroForm');
+    if (registroForm) {
+        registroForm.addEventListener('submit', function(event) { // Volvemos a usar addEventListener pero con nuestro id "Submit"
+            event.preventDefault(); // Evitar que el formulario se envíe
+
+            var nickname = document.getElementById('nickname').value;
+            if (nickname) {
+                // Almacenar el nickname en el almacenamiento local
+                localStorage.setItem('nickname', nickname);
+                // Redirigir a la página de la trivia
+                window.location.href = 'azul.html';
+            } else {
+                alert('Por favor, ingresa un nickname.');
+            }
+        });
+    }
+   
+})
+    // Manejo de la página de trivia.html
+    // var triviaSaludo = document.getElementById('triviaSaludo');
+    // if (triviaSaludo) {
+    //     var nickname = localStorage.getItem('nickname');
+    //     if (nickname) {
+    //         // Mostrar un saludo personalizado con el nickname
+    //         triviaSaludo.textContent = 'Bienvenido a la Trivia, ' + nickname + ' !'+'Donde compites con el tiempo y  conoces los:';
+    //     } else {
+    //         // Redirigir al usuario a la página de registro si no se ha registrado
+    //         window.location.href = '../index.html';
+    //     }
+    // }
+
+    // Limpiar nickname
+    // var limpiarNicknameBtn = document.getElementById('limpiarNickname');
+    // if (limpiarNicknameBtn) {
+    //     limpiarNicknameBtn.addEventListener('click', function() {
+    //         // Limpiar el nickname almacenado en el almacenamiento local
+    //         localStorage.removeItem('nickname');
+    //         window.location.href = '../azul.html';
+    //         alert('Nickname limpiado.');
+    //     });
+    // }
+
+//Selectors
+const txtMsg = document.getElementById("msg");
+const count = document.getElementById("counter");
+const offset = document.getElementById("offset");
+const btnCipher = document.getElementById("cipher");
+const btnDecipher = document.getElementById("decipher");
+const lblMsgResult = document.getElementById("lblMsgResult");
+const txtMsgResult = document.getElementById("msgResult");
+const btnCopy = document.getElementById("copy");
+const modalC = document.getElementsByClassName("modalContainer")[0];
+const modal = document.getElementsByClassName("modal")[0];
+const close = document.getElementById("close");
+
+//EventListeners
+/* Limit characters*/
+txtMsg.addEventListener("keyup", () =>{
+    count.innerHTML = txtMsg.value.length + "/280";
+});
+
+/* Function cipher*/
+btnCipher.addEventListener("click",()=>{
+    if(txtMsg.value == ""){
+        alert("Ingresa tu mensaje secreto.");
+    }else if(offset.value == ""){
+        alert("No olvides ingresar tu clave secreta.");
+    }else{
+        lblMsgResult.innerHTML = "Su mensaje cifrado es ";
+        let msgResult = cipher.encode(parseInt(offset.value),txtMsg.value);
+        txtMsgResult.innerHTML = msgResult;
+        openModal();
+    }
+});
+
+/*Function decipher*/
+btnDecipher.addEventListener("click",()=>{
+    if(txtMsg.value == ""){
+        alert("Ingresa tu mensaje secreto.");
+    }else if(offset.value == ""){
+        alert("No olvides ingresar tu clave secreta.");
+    }else{
+        lblMsgResult.innerHTML = "Su mensaje descifrado es ";
+        let msgResult = cipher.decode(parseInt(offset.value),txtMsg.value);
+        txtMsgResult.innerHTML = msgResult;
+        openModal();
+    }
+});
+
+/* Copy cipher or decipher message */
+btnCopy.addEventListener("click",()=>{
+    txtMsgResult.select();
+    document.execCommand("copy");
+    setTimeout(()=>{
+        btnCopy.textContent = "Copiado!";
+    }, 100);
+});
+
+/* Modal events*/
+close.addEventListener("click",()=>{
+    closeModal();
+});
+
+window.addEventListener("click",(e)=>{
+    if(e.target == modalC){
+        closeModal();
+    }
+});
+
+/********Functions */
+function openModal(){
+    modalC.classList.remove("containerClose");
+    modal.classList.remove("modalClose");
+}
+
+function closeModal(){
+    modal.classList.add("modalClose");
+    clearMsg();
+    setTimeout(()=>{
+        btnCopy.innerHTML = "<i class='fas fa-copy'></i> Copiar";
+        modalC.classList.add("containerClose");
+    }, 550);
+}
+
+function clearMsg(){
+    txtMsg.value = "";
+    txtMsg.innerHTML = "";
+    count.innerHTML = "0/280";
+}
